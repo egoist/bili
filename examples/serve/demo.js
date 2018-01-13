b2c75eaeeb659247ba1e11bc73072bb71c833bf8
EGOIST <0x142857@gmail.com>
2018-01-13 14:48:11 +0800

    add example for serve command

---
+++ examples/serve/demo.js
@@ -0,0 +1,5 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import MyComponent from './'
+
+ReactDOM.render(<MyComponent />, document.getElementById('app'))
---
+++ examples/serve/index.js
@@ -0,0 +1,4 @@
+import React from 'react'
+
+export default () => <h1>My Component</h1>
+
