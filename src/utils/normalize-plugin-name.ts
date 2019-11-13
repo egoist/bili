70f216e51c416227e791d06c68a1d1b5dfad28db
Dario Vladovic <d.vladimyr@gmail.com>
2019-11-14 00:55:17 +0100

    feat: recognize official @rollup plugins

--- src/utils/normalize-plugin-name.ts
+++ src/utils/normalize-plugin-name.ts
@@ -1,6 +1,10 @@
 export default function(name: string): string {
+  // @rollup/alias => @rollup/plugin-alias
+  // @foo/baz => @foo/rollup-plugin-baz
   if (/^@[^/]+\//.test(name)) {
-    return name.replace(/^@([^/]+)\/(rollup-plugin-)?/, '@$1/rollup-plugin-')
+    return name.replace(/^@([^/]+)\/(rollup-)?(plugin-)?/, (_, m1) => {
+      return m1 === 'rollup' ? `@rollup/plugin-` : `@${m1}/rollup-plugin-`
+    })
   }
 
   return name.replace(/^(rollup-plugin-)?/, 'rollup-plugin-')
