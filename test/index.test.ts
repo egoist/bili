79bb22cc544cfeff301b897c43edcaabc6095025
Dario Vladovic <d.vladimyr@gmail.com>
2019-11-14 21:05:46 +0100

    test: load scoped plugins

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,5 +1,6 @@
 import path from 'path'
 import { Bundler, Config, Options } from '../src'
+import { RollupConfig } from '../src/types'
 
 process.env.BABEL_ENV = 'anything-not-test'
 
@@ -14,6 +15,24 @@ function generate(config: Config, options: Options) {
     ...options
   })
   return bundler.run()
+}
+
+function getRollupConfig(
+  config: Config,
+  options: Options
+): Promise<RollupConfig> {
+  return new Promise(resolve => {
+    generate(
+      {
+        ...config,
+        extendRollupConfig: config => {
+          resolve(config)
+          return config
+        }
+      },
+      options
+    )
+  })
 }
 
 function snapshot(
@@ -42,6 +61,34 @@ function snapshot(
     }
   })
 }
+
+test('resolve scoped plugins', async () => {
+  const { inputConfig } = await getRollupConfig(
+    {
+      input: 'index.js',
+      plugins: {
+        '@foo/bar': true
+      }
+    },
+    { rootDir: fixture('defaults') }
+  )
+  expect(
+    inputConfig.plugins.find(plugin => plugin.name === '@foo/bar')
+  ).toBeTruthy()
+})
+
+test('resolve @rollup plugins', async () => {
+  const { inputConfig } = await getRollupConfig(
+    {
+      input: 'index.js',
+      plugins: {
+        '@rollup/baz': true
+      }
+    },
+    { rootDir: fixture('defaults') }
+  )
+  expect(inputConfig.plugins.find(plugin => plugin.name === 'baz')).toBeTruthy()
+})
 
 snapshot({
   title: 'defaults',
